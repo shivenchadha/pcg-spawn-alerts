c378360a2c162d17d66459da58eff72031d9c0bc
Shiven Chadha <shivenchadha@gmail.com>
2025-11-02 17:49:15 +0530

    Add config.py template

---
+++ config.py
@@ -0,0 +1,5 @@
+# Replace with your Discord webhook URL
+webhook_url = "DISCORD_WEBHOOK_URL"
+
+# Pokédex IDs of Pokémon you want alerts for
+pokemon_watchlist = {6, 384, 493}  # Example IDs: Charizard, Rayquaza, Arceus
